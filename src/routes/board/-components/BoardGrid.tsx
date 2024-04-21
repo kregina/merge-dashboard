3a1a6560dd25dd7526060c6ac03dda7a1e5e9577
Kelly Silva <kellynha756@gmail.com>
2024-04-21 23:52:49 +0300

    Refactor BoardGrid

--- src/routes/board/-components/BoardGrid.tsx
+++ src/routes/board/-components/BoardGrid.tsx
@@ -1,23 +1,13 @@
-import { Card, Dialog, DialogContent } from '@/components';
-import { swapArrayItems } from '@/lib/swapArrayItems';
-import { cn } from '@/lib/utils';
-import { Item } from '@/services';
-import { motion } from 'framer-motion';
-import { useMemo, useState } from 'react';
-import { useBoardContext } from './BoardContext';
-import { BoardItem } from './BoardItem/BoardItem';
-import { BoardItemAdd } from './BoardItem/BoardItemAdd';
-import { BoardItemEdit } from './BoardItem/BoardItemEdit';
+import { Card } from '@/components';
+import { Board } from '@/services';
+import { FC, ReactNode, useMemo } from 'react';
 
-export const BoardGrid = () => {
-  const context = useBoardContext();
+interface BoardGridProps {
+  children: ReactNode;
+  board: Board;
+}
 
-  const [board, setBoard] = useState(context.board);
-  const [draggingIndex, setDraggingIndex] = useState(-1);
-  const [draggingOverIndex, setDraggingOverIndex] = useState(-1);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedIndex, setSelectedIndex] = useState(-1);
-
+export const BoardGrid: FC<BoardGridProps> = ({ children, board }) => {
   const gridStyle = useMemo(
     () => ({
       gridTemplateColumns: `repeat(${board.width}, minmax(0, 1fr))`,
@@ -26,133 +16,14 @@ export const BoardGrid = () => {
     [board.width, board.height],
   );
 
-  // const isHovered = hoveredId === item?.chainId;
-
-  const itemClasses = useMemo(
-    () =>
-      cn(
-        'rounded cursor-grab transition-colors duration-100 ease-in-out relative overflow-hidden h-full',
-        // isActive && 'bg-emerald-600 dark:bg-emerald-800',
-        // isHovered && 'bg-orange-500 dark:bg-orange-700',
-      ),
-    [],
-  );
-
-  const handleOnDragStart = (index: number) => {
-    setDraggingIndex(index);
-  };
-
-  const handleOnDragOver = (
-    e: React.DragEvent<HTMLDivElement>,
-    index: number,
-  ) => {
-    e.preventDefault();
-    setDraggingOverIndex(index);
-  };
-
-  const handleOnDrop = () => {
-    const swappedArray = swapArrayItems(
-      board.items,
-      draggingIndex,
-      draggingOverIndex,
-    );
-    setBoard({ ...board, items: swappedArray });
-  };
-
-  const onAddItem = (addedItem: Item, index: number) => {
-    if (addedItem) {
-      setBoard((prevBoard) => {
-        const updatedItems = [
-          ...prevBoard.items.slice(0, index),
-          addedItem,
-          ...prevBoard.items.slice(index + 1),
-        ];
-        return { ...prevBoard, items: updatedItems };
-      });
-      setSelectedIndex(-1);
-    }
-  };
-
-  const onDelete = (itemId: string, index: number) => {
-    if (itemId) {
-      const emptyItem = {
-        itemId: itemId,
-      } as Item;
-
-      const newBoard = [
-        ...board.items.slice(0, index),
-        emptyItem,
-        ...board.items.slice(index + 1),
-      ];
-
-      setBoard({ ...board, items: newBoard });
-      setSelectedIndex(-1);
-    }
-  };
-
-  const onUpdate = (updatedItem: Item) => {
-    if (updatedItem) {
-      const newItems = board.items.map((item) =>
-        item?.itemId === updatedItem.itemId ? updatedItem : item,
-      );
-      setBoard({ ...board, items: newItems });
-      setSelectedIndex(-1);
-    }
-  };
-
   return (
-    <div className="m-auto">
-      <Card>
-        <div
-          className="w-full h-full grid border-4 border-[#c2a061] bg-[#e4dccc] rounded-xl p-1 gap-1"
-          style={gridStyle}
-        >
-          {board.items.map((item, index) => (
-            <div
-              key={item.itemId}
-              className="col-span-1 p-1 even:bg-[#e4dccc] odd:bg-[#cec6af] rounded lg:w-[90px]"
-            >
-              <Dialog open={selectedIndex === index}>
-                <motion.div
-                  draggable={!!item?.itemType}
-                  layout
-                  className={cn(itemClasses)}
-                  onDrop={handleOnDrop}
-                  onDragStart={() => handleOnDragStart(index)}
-                  onDragOver={(e) => handleOnDragOver(e, index)}
-                >
-                  <div
-                    className="h-full w-full "
-                    onClick={() => {
-                      setSelectedIndex(index);
-                    }}
-                  >
-                    <BoardItem item={item} />
-                  </div>
-                </motion.div>
-
-                <DialogContent>
-                  <Card>
-                    {item.itemType ? (
-                      <BoardItemEdit
-                        item={item}
-                        setDeleteItem={(item) => onDelete(item.itemId, index)}
-                        setOnSaveItem={(item) => onUpdate(item)}
-                      />
-                    ) : (
-                      <BoardItemAdd
-                        setAddedItem={(addedItem) =>
-                          onAddItem(addedItem, index)
-                        }
-                      />
-                    )}
-                  </Card>
-                </DialogContent>
-              </Dialog>
-            </div>
-          ))}
-        </div>
-      </Card>
-    </div>
+    <Card>
+      <div
+        className="w-full h-full grid border-4 border-[#c2a061] bg-[#e4dccc] rounded-xl p-1 gap-1"
+        style={gridStyle}
+      >
+        {children}
+      </div>
+    </Card>
   );
 };
--- src/routes/board/index.tsx
+++ src/routes/board/index.tsx
@@ -1,7 +1,15 @@
+import { Card, Dialog, DialogContent } from '@/components';
+import { swapArrayItems } from '@/lib/swapArrayItems';
+import { cn } from '@/lib/utils';
+import { Item } from '@/services';
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { createFileRoute } from '@tanstack/react-router';
-import { Board } from './-components/Board';
-import { BoardContext } from './-components/BoardContext';
+import { motion } from 'framer-motion';
+import { useMemo, useState } from 'react';
+import { BoardGrid } from './-components/BoardGrid';
+import { BoardItem } from './-components/BoardItem/BoardItem';
+import { BoardItemAdd } from './-components/BoardItem/BoardItemAdd';
+import { BoardItemEdit } from './-components/BoardItem/BoardItemEdit';
 import { getBoardQueryOptions } from './-components/boardQueryOptions';
 
 export const Route = createFileRoute('/board/')({
@@ -12,17 +20,130 @@ export const Route = createFileRoute('/board/')({
 
 function BoardComponent() {
   const boardQuery = useSuspenseQuery(getBoardQueryOptions);
-  const board = boardQuery.data;
+
+  const [board, setBoard] = useState(boardQuery.data);
+  const [draggingIndex, setDraggingIndex] = useState(-1);
+  const [draggingOverIndex, setDraggingOverIndex] = useState(-1);
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  // const isHovered = hoveredId === item?.chainId;
+
+  const itemClasses = useMemo(
+    () =>
+      cn(
+        'rounded cursor-grab transition-colors duration-100 ease-in-out relative overflow-hidden h-full',
+        // isActive && 'bg-emerald-600 dark:bg-emerald-800',
+        // isHovered && 'bg-orange-500 dark:bg-orange-700',
+      ),
+    [],
+  );
+
+  const handleOnDragStart = () => {
+    setDraggingIndex(selectedIndex);
+  };
+
+  const handleOnDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setDraggingOverIndex(selectedIndex);
+  };
+
+  const handleOnDrop = () => {
+    const swappedArray = swapArrayItems(
+      board.items,
+      draggingIndex,
+      draggingOverIndex,
+    );
+    setBoard({ ...board, items: swappedArray });
+  };
+
+  const onAddItem = (addedItem: Item) => {
+    if (addedItem) {
+      const newBoard = board.items.map((item, i) => {
+        if (i === selectedIndex) {
+          return addedItem;
+        }
+        return item;
+      });
+
+      setBoard({ ...board, items: newBoard });
+      setSelectedIndex(-1);
+    }
+  };
+
+  const onDelete = (itemId: string) => {
+    if (itemId) {
+      const emptyItem = {
+        itemId: itemId,
+      } as Item;
+
+      const newBoard = board.items.map((item, i) => {
+        if (i === selectedIndex) {
+          return emptyItem;
+        }
+        return item;
+      });
+
+      setBoard({ ...board, items: newBoard });
+      setSelectedIndex(-1);
+    }
+  };
+
+  const onUpdate = (updatedItem: Item) => {
+    if (updatedItem) {
+      const newItems = board.items.map((item) =>
+        item?.itemId === updatedItem.itemId ? updatedItem : item,
+      );
+      setBoard({ ...board, items: newItems });
+      setSelectedIndex(-1);
+    }
+  };
 
   return (
-    <BoardContext.Provider
-      value={{
-        board,
-        itemsHistory: [],
-        selectedIndex: -1,
-      }}
-    >
-      <Board />
-    </BoardContext.Provider>
+    <div className="m-auto">
+      <BoardGrid board={board}>
+        {board.items.map((item, index) => (
+          <div
+            key={item.itemId}
+            className="col-span-1 p-1 even:bg-[#e4dccc] odd:bg-[#cec6af] rounded lg:w-[90px]"
+          >
+            <Dialog open={selectedIndex === index}>
+              <motion.div
+                draggable={!!item?.itemType}
+                layout
+                className={cn(itemClasses)}
+                onDrop={handleOnDrop}
+                onDragStart={() => handleOnDragStart()}
+                onDragOver={(e) => handleOnDragOver(e)}
+              >
+                <div
+                  className="h-full w-full "
+                  onClick={() => {
+                    setSelectedIndex(index);
+                  }}
+                >
+                  <BoardItem item={item} />
+                </div>
+              </motion.div>
+
+              <DialogContent>
+                <Card>
+                  {item.itemType ? (
+                    <BoardItemEdit
+                      item={item}
+                      setDeleteItem={(item) => onDelete(item.itemId)}
+                      setOnSaveItem={(item) => onUpdate(item)}
+                    />
+                  ) : (
+                    <BoardItemAdd
+                      setAddedItem={(addedItem) => onAddItem(addedItem)}
+                    />
+                  )}
+                </Card>
+              </DialogContent>
+            </Dialog>
+          </div>
+        ))}
+      </BoardGrid>
+    </div>
   );
 }
