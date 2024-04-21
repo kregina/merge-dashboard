50a35086a6eecea1f113a371a7697caea1b7e3d9
Kelly Silva <kellynha756@gmail.com>
2024-04-22 02:09:09 +0300

    Clean service

--- src/services/board.service.test.ts
+++ src/services/board.service.test.ts
@@ -1,12 +1,5 @@
 import { describe, expect, it, vi } from 'vitest';
-import {
-  Board,
-  Item,
-  addItemToBoard,
-  deleteItemFromBoard,
-  fetchBoard,
-  updateItemToBoard,
-} from './board.service';
+import { Board, Item, fetchBoard } from './board.service';
 
 // Mock uuid and delay
 vi.mock('uuid', () => ({
@@ -40,41 +33,6 @@ const board: Board = {
 describe('fetchBoard', () => {
   it('should fetch the board with items containing unique IDs', async () => {
     const result = await fetchBoard();
-    expect(result.items[0]?.itemId).toBe('unique-id');
-  });
-});
-
-describe('addItemToBoard', () => {
-  it('should add an item at specified index', async () => {
-    const newItem = { ...sampleItem, itemId: 'item-2' };
-    const result = await addItemToBoard({
-      index: 1,
-      newItem,
-      boardItems: board.items,
-    });
-    expect(result).toHaveLength(2);
-    expect(result[1]).toEqual(newItem);
-  });
-});
-
-describe('updateItemToBoard', () => {
-  it('should update an item based on its ID', async () => {
-    const updatedItem = { ...sampleItem, itemLevel: 2 };
-    const result = await updateItemToBoard({
-      updatedItem,
-      boardItems: board.items,
-    });
-    expect(result[0]?.itemLevel).toBe(2);
-  });
-});
-
-describe('deleteItemFromBoard', () => {
-  it('should delete an item by setting it to null at the specified index', async () => {
-    const result = await deleteItemFromBoard({
-      itemId: 'item-1',
-      boardItems: board.items,
-      index: 0,
-    });
-    expect(result[0]?.itemId).toBe('item-1');
+    expect(result.items[0].itemId).toBe('unique-id');
   });
 });
