64000515de6e569585c2b19b103ed427ea67d2fa
Kelly Silva <kellynha756@gmail.com>
2024-04-22 13:52:06 +0300

    Fix basepath

--- src/main.tsx
+++ src/main.tsx
@@ -22,7 +22,7 @@ const router = createRouter({
   },
   defaultPreload: 'intent',
   defaultPreloadStaleTime: 0,
-  basepath: '/merge-dashboard',
+  basepath: '/merge-dashboard/',
 });
 
 // Register the router instance for type safety
